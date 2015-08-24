221ee31bac788bd4e632b8171bf7c8ad59e8dcd4
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-24 16:34:15 -0700

    adds first routes for index, add a category and display all categories on index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 import java.util.*;
 
@@ -14,10 +17,30 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("categories", Category.all()); 
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/categories/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/category-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());	
 
-  }
+    post("/categories", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Category newCategory = new Category(name);
+      newCategory.save();
+      model.put("Category", newCategory);
+      //only put .all if you want to return directly from post route to index with no reload/refresh
+     // model.put("words", Word.all());
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+
+  }//end of main
 }
